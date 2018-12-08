fcfdb79a3a3c43c5e8e27bd4c790ed424ba8c112
evanboissonnot <evan@boissonnot.fr>
2018-12-08 10:20:24 +0100

    update to use config, but miss default app.js

--- routes/contact.js
+++ routes/contact.js
@@ -1,14 +1,19 @@
-const express = require("express");
+//const API = require('../activecampainloader/libs/index').API;
+const express = require("express"); 
+//const config = require('config');
 const router = express.Router();
+
 
 router.post('/', function(req, res, next) {
     let toBeAddedContact = req.body;
     let status = 422;
 
+    //console.log(config.api.activecampaign.key);
+
     if(toBeAddedContact && toBeAddedContact.email) {
         status = 200;
 
-        throw new Exception("07/12/2018, A finir");
+        
     }
 
     res.status(status).send({
