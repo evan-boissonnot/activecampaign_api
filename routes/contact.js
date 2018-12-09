6766f6559e896931ad5a866cf45a0bac109aea44
evan-boissonnot <commercial@boissonnot.Fr>
2018-12-09 22:16:42 +0100

    prepare to use api activecampaign

--- routes/contact.js
+++ routes/contact.js
@@ -1,21 +1,34 @@
 const express = require("express");
-const apiActiveCampaign = require("../activecampainloader/libs/index").API;
+const ApiActiveCampaign = require("../activecampainloader/libs/index").API;
 const router = express.Router();
 
+const activecampainAPI = new ApiActiveCampaign(process.env.ACTIVECAMPAIGN_DOMAIN, 
+                                               process.env.ACTIVECAMPAIGN_TOKEN);
+
 router.post('/', function(req, res, next) {
-    let toBeAddedContact = req.body;
+    let toBeAdded = req.body;
     let status = 422;
+    let errorMessage = "";
 
-    if(toBeAddedContact && toBeAddedContact.email) {
-        status = 200;
-
-        throw new Error("07/12/2018, A finir");
+    if(toBeAddedContact && toBeAddedContact.contact) {
+        let fieldId = 0;
+        let automationId = 0;
+        activecampainAPI.addContactWithCustomFieldToAutomation(toBeAdded.contact, 
+                                                               fieldId, 
+                                                               toBeAdded.url,
+                                                               automationId)
+                        .then((result) => {
+                            res.status(200).send({
+                                item: result,
+                                isSuccess: true
+                            });
+                        });
+    } else {
+        res.status(status).send({
+            item: null,
+            isSuccess: false
+        });
     }
-
-    res.status(status).send({
-        item: toBeAddedContact,
-        isSuccess: status == 200
-    });
 });
 
 module.exports = router;
