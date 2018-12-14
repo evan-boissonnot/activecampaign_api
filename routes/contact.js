68a9b56b3de896814efb9d406941221f16ba8140
evan-boissonnot <commercial@boissonnot.Fr>
2018-12-14 08:26:51 +0100

    use config package and setting environment variables

--- routes/contact.js
+++ routes/contact.js
@@ -1,7 +1,18 @@
 
 const express = require("express");
-const ApiActiveCampaign = require("../activecampainloader/libs/index").API;
+const ApiActiveCampaign = require("../packages/activecampainloader/libs/index").API;
 const router = express.Router();
+const config = require('config');
+const AUTOMATION_ID_KEY = "api.activecampaign.automation.id";
+const AUTOMATION_FIELD_ID_KEY = "api.activecampaign.automation.fieldId";
+
+if (! config.has(AUTOMATION_ID_KEY)) {
+    throw new Error("AUTOMATION ID not define in configuration");
+}
+
+if (! config.has(AUTOMATION_FIELD_ID_KEY)) {
+    throw new Error("AUTOMATION FIELD ID not define in configuration");
+}
 
 const activecampainAPI = new ApiActiveCampaign(process.env.ACTIVECAMPAIGN_DOMAIN, 
                                                process.env.ACTIVECAMPAIGN_TOKEN);
@@ -11,10 +22,17 @@ router.post('/', function(req, res, next) {
     let toBeAdded = req.body;
     let status = 422;
     let errorMessage = "";
-    
-    if(toBeAddedContact && toBeAddedContact.contact) {
-        let fieldId = 0;
-        let automationId = 0;
+
+    console.log("contact/index");
+    console.log(toBeAdded);
+
+    if(toBeAdded && toBeAdded.contact && toBeAdded.contact.email) {
+        let fieldId = config.get(AUTOMATION_FIELD_ID_KEY);
+        let automationId = config.get(AUTOMATION_ID_KEY);
+
+        console.log("fieldId : " + fieldId);
+        console.log("automationId : " + automationId);
+
         activecampainAPI.addContactWithCustomFieldToAutomation(toBeAdded.contact, 
                                                                fieldId, 
                                                                toBeAdded.url,
@@ -28,7 +46,8 @@ router.post('/', function(req, res, next) {
     } else {
         res.status(status).send({
             item: null,
-            isSuccess: false
+            isSuccess: false,
+            message: "bad content"
         });
     }
 });
